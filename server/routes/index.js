b1c298c6fa476538ea13a74b2eb6ee93f605fb9a
ayshabilaii <aysha.bilai@ontariotechu.net>
2024-11-22 11:17:41 -0500

    commits

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+let Shoe = require('../model/shoe.js');
+const shoe = require('../model/shoe.js');
 /* GET index page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
@@ -9,12 +10,31 @@ router.get('/', function(req, res, next) {
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
-/* GET products page. */
-router.get('/collection', function(req, res, next) {
-  res.render('collection', { title: 'Collection' });
-});
+
+/* GET collection page. 
+router.get('/collection', function(req, res, next) { /*router gets requests for /collection path*/
+  //const ShoeList = [ /*empty array to hold shoe info*/
+ /* ];
+  res.render('collection', { title: 'Collection', ShoeList: ShoeList });
+});*/
+
+router.get('/collection',async(req,res,next)=>{
+  try{
+      const ShoeList = await Shoe.find();
+      res.render('collection',{
+          title:'Collection',
+          ShoeList:ShoeList
+      })}
+      catch(err){
+          console.error(err);
+          res.render('shoe/list',{
+              error:'Error on the server'
+          })
+      }
+      });
+
 /* GET service page. */
-router.get('/service', function(req, res, next) {
+router.get('/shoes', function(req, res, next) {
   res.render('index', { title: 'Shoes' });
 });
 /* GET contactus page. */
